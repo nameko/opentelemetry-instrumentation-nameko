cb58bbbd78bf2fac9562694ffed2e315a30a8fa0
Matt Yule-Bennett <matt@bennett.name>
2021-06-28 13:44:41 +0100

    setup boilerplate

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 from setuptools import find_packages, setup
 
+
 setup(
-    name='nameko-opentelemetry',
-    version='1.0.0',
-    description='Nameko extension producing opentelemetry data',
-    author='Nameko Authors',
-    url='https://github.com/nameko/nameko-opentelemetry',
-    packages=find_packages(exclude=['test', 'test.*']),
-    install_requires=[
-        "nameko>=2.8.5",
-    ],
-    extras_require={
-        'dev': [
-            "coverage",
-            "pytest",
-        ]
-    },
+    name="nameko-opentelemetry",
+    version="1.0.0",
+    description="Nameko extension producing opentelemetry data",
+    author="Nameko Authors",
+    url="https://github.com/nameko/nameko-opentelemetry",
+    packages=find_packages(exclude=["test", "test.*"]),
+    install_requires=["nameko>=3", "opentelemetry-api",],
+    extras_require={"dev": ["coverage", "pytest", "opentelemetry-sdk"]},
     dependency_links=[],
     zip_safe=True,
-    license='Apache License, Version 2.0'
+    license="Apache License, Version 2.0",
 )
