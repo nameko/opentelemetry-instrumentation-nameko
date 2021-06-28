95db60d2058c6ff0412775a3452c3a76f55a716d
Matt Yule-Bennett <matt@bennett.name>
2021-06-28 20:56:34 +0100

    move result attributes tests to own class

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -12,6 +12,38 @@ from nameko_opentelemetry.entrypoints import TRUNCATE_MAX_LENGTH
 
 class TestSpanAttributes:
     @pytest.fixture
+    def container(self, container_factory):
+        class Service:
+            name = "service"
+
+            @rpc
+            def method(self, arg, kwarg=None):
+                return "OK"
+
+        container = container_factory(Service)
+        container.start()
+
+        return container
+
+    def test_common(self, container, memory_exporter):
+
+        with entrypoint_hook(container, "method") as hook:
+            assert hook("arg", kwarg="kwarg") == "OK"
+
+        spans = memory_exporter.get_finished_spans()
+        assert len(spans) == 1
+
+        assert spans[0].name == "service.method"
+
+        attributes = spans[0].attributes
+        assert attributes["hostname"] == socket.gethostname()
+        assert attributes["service_name"] == "service"
+        assert attributes["entrypoint_type"] == "Rpc"
+        assert attributes["method_name"] == "method"
+
+
+class TestResultAttributes:
+    @pytest.fixture
     def unserializable_object(self):
         return object()
 
@@ -35,32 +67,12 @@ class TestSpanAttributes:
             def raises(self, arg, kwarg=None):
                 raise self.Error("boom")
 
-            @rpc(expected_exceptions=Error)
-            def raises_expected(self, arg, kwarg=None):
-                raise self.Error("boom")
-
         container = container_factory(Service)
         container.start()
 
         return container
 
-    def test_common(self, container, memory_exporter):
-
-        with entrypoint_hook(container, "method") as hook:
-            assert hook("arg", kwarg="kwarg") == "OK"
-
-        spans = memory_exporter.get_finished_spans()
-        assert len(spans) == 1
-
-        assert spans[0].name == "service.method"
-
-        attributes = spans[0].attributes
-        assert attributes["hostname"] == socket.gethostname()
-        assert attributes["service_name"] == "service"
-        assert attributes["entrypoint_type"] == "Rpc"
-        assert attributes["method_name"] == "method"
-
-    def test_success(self, container, memory_exporter):
+    def test_simple(self, container, memory_exporter):
 
         with entrypoint_hook(container, "method") as hook:
             assert hook("arg", kwarg="kwarg") == "OK"
@@ -71,6 +83,19 @@ class TestSpanAttributes:
         attributes = spans[0].attributes
         assert attributes["method_name"] == "method"
         assert attributes["result"] == "OK"
+
+    def test_exception(self, container, memory_exporter):
+
+        with entrypoint_hook(container, "raises") as hook:
+            with pytest.raises(Exception):
+                hook("arg", kwarg="kwarg")
+
+        spans = memory_exporter.get_finished_spans()
+        assert len(spans) == 1
+
+        attributes = spans[0].attributes
+        assert attributes["method_name"] == "raises"
+        assert attributes.get("result") is None
 
     def test_unserializable_result(
         self, container, memory_exporter, unserializable_object
