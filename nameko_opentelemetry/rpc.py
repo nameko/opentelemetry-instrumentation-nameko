65eae786231fd7b3a8a0f9c712d49cfd318568c2
Matt Yule-Bennett <matt@bennett.name>
2021-07-01 11:45:07 +0100

    tests and impl for rpc exceptions where no entrypoint is fired

--- nameko_opentelemetry/rpc.py
+++ nameko_opentelemetry/rpc.py
@@ -2,9 +2,11 @@ from functools import partial
 from weakref import WeakKeyDictionary
 
 import nameko.rpc
+from nameko.exceptions import IncorrectSignature, MethodNotFound
 from opentelemetry import trace
 from opentelemetry.instrumentation.utils import unwrap
 from opentelemetry.propagate import inject
+from opentelemetry.trace.status import Status, StatusCode
 from opentelemetry.util._time import _time_ns
 from wrapt import wrap_function_wrapper
 
@@ -40,13 +42,51 @@ def get_response(tracer, wrapped, instance, args, kwargs):
     return resp
 
 
+def consumer_handle_message(tracer, wrapped, instance, args, kwargs):
+    body, message = args
+    routing_key = message.delivery_info["routing_key"]
+    try:
+        instance.get_provider_for_method(routing_key)
+    except MethodNotFound as exc:
+        span = tracer.start_span(routing_key, kind=trace.SpanKind.SERVER)
+        span.set_status(Status(StatusCode.ERROR, description=f"MethodNotFound: {exc}"))
+        with trace.use_span(span, end_on_exit=True):
+            return wrapped(*args, **kwargs)
+    else:
+        return wrapped(*args, **kwargs)
+
+
+def entrypoint_handle_message(tracer, wrapped, instance, args, kwargs):
+    body, message = args
+    try:
+        instance.check_signature(body.get("args"), body.get("kwargs"))
+    except IncorrectSignature as exc:
+        name = f"{instance.container.service_name}.{instance.method_name}"
+        span = tracer.start_span(name, kind=trace.SpanKind.SERVER)
+        span.set_status(Status(StatusCode.ERROR, description=f"MethodNotFound: {exc}"))
+        with trace.use_span(span, end_on_exit=True):
+            return wrapped(*args, **kwargs)
+    else:
+        return wrapped(*args, **kwargs)
+
+
 def instrument(tracer):
     wrap_function_wrapper("nameko.rpc", "Client._call", partial(initiate_call, tracer))
     wrap_function_wrapper(
         "nameko.rpc", "RpcCall.get_response", partial(get_response, tracer)
+    )
+    wrap_function_wrapper(
+        "nameko.rpc",
+        "RpcConsumer.handle_message",
+        partial(consumer_handle_message, tracer),
+    )
+    wrap_function_wrapper(
+        "nameko.rpc", "Rpc.handle_message", partial(entrypoint_handle_message, tracer),
     )
 
 
 def uninstrument():
     unwrap(nameko.rpc.Client, "_call")
     unwrap(nameko.rpc.RpcCall, "get_response")
+    unwrap(nameko.rpc.RpcConsumer, "handle_message")
+    unwrap(nameko.rpc.Rpc, "handle_message")
--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -1,11 +1,13 @@
 from unittest.mock import Mock
 
 import pytest
+from nameko.exceptions import IncorrectSignature, MethodNotFound
 from nameko.rpc import ServiceRpc, rpc
 from nameko.standalone.rpc import ServiceRpcClient
 from nameko.testing.services import entrypoint_waiter
 from nameko.testing.utils import get_extension
 from opentelemetry.trace import SpanKind
+from opentelemetry.trace.status import StatusCode
 
 
 class TestCaptureIncomingContext:
@@ -53,10 +55,55 @@ class TestNoEntrypointFired:
     """ Test cases where the request is aborted before finding an entrypoint
     """
 
-    pass
+    @pytest.fixture
+    def container(self, container_factory, rabbit_config):
+        class Service:
+            name = "service"
+
+            @rpc
+            def method(self, arg, kwarg=None):
+                return "OK"
+
+        container = container_factory(Service)
+        container.start()
+
+        return container
+
+    @pytest.fixture
+    def client(self, rabbit_config):
+        with ServiceRpcClient("service") as client:
+            yield client
+
+    def test_method_not_found(self, container, client, memory_exporter):
+
+        with pytest.raises(MethodNotFound):
+            client.not_a_method()
+
+        container.stop()
+
+        spans = memory_exporter.get_finished_spans()
+        assert len(spans) == 2
+
+        server_span = list(filter(lambda span: span.kind == SpanKind.SERVER, spans))[0]
+
+        assert not server_span.status.is_ok
+        assert server_span.status.status_code == StatusCode.ERROR
+        assert server_span.status.description == "MethodNotFound: not_a_method"
+
+    def test_incorrect_signature(self, container, client, memory_exporter):
+        with pytest.raises(IncorrectSignature):
+            client.method("foo", "bar", "baz")
+
+        container.stop()
+
+        spans = memory_exporter.get_finished_spans()
+        assert len(spans) == 2
+
+        server_span = list(filter(lambda span: span.kind == SpanKind.SERVER, spans))[0]
+
+        assert not server_span.status.is_ok
+        assert server_span.status.status_code == StatusCode.ERROR
+        assert "IncorrectSignature" in server_span.status.description
 
 
-# 1. add this for rpc, messaging, events
-# 2. include addtl patches for not found in rpc case
-# 3. commit, along with existing impls (without attribute customization)
 # 4. think about then implement attribute customization for rpc, events, messaging, timer
