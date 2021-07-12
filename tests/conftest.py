0fa3f1c65552c5955a4c3364bf6eb3f7f0ec2de2
Matt Yule-Bennett <matt@bennett.name>
2021-07-12 11:21:49 +0100

    filter out any spans that are requests to rabbit mgmt
    the rabbit_config fixture removes vhosts used by a test after that
    test has completed, to speed up the test suite. these occasionally get
    caught by instrumentation in subsequent tests, which casues some
    flakiness

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,36 @@
 # -*- coding: utf-8 -*-
 import pytest
 from opentelemetry import trace
+from opentelemetry.instrumentation.utils import unwrap
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+from wrapt import wrap_function_wrapper
 
 from nameko_opentelemetry import NamekoInstrumentor
 
 
 @pytest.fixture(scope="session")
-def memory_exporter():
+def filter_spans():
+    def filter_rabbit(span):
+        if "api/vhosts/nameko_test" not in span.attributes.get("http.url", ""):
+            return True
+
+    def get_finished_spans(wrapped, instance, args, kwargs):
+        spans = wrapped(*args, **kwargs)
+        return list(filter(filter_rabbit, spans))
+
+    wrap_function_wrapper(
+        "opentelemetry.sdk.trace.export.in_memory_span_exporter",
+        "InMemorySpanExporter.get_finished_spans",
+        get_finished_spans,
+    )
+    yield
+    unwrap(InMemorySpanExporter, "get_finished_spans")
+
+
+@pytest.fixture(scope="session")
+def memory_exporter(filter_spans):
     return InMemorySpanExporter()
 
 
